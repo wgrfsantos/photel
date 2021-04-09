fc2f043c7cd3ae6c02780056375b2ca30b462ce5
Claudio Rocha <claudio.rocha.sccp@gmail.com>
2021-04-10 00:55:59 +0100

    fix-validação de login

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -1,53 +1,64 @@
 <?php
-
 namespace Controllers;
 
-use Core\Controller;
-use Models\Users;
+use \Core\Controller;
+use \Models\Users;
 
-class LoginController extends Controller
-{
+class LoginController extends Controller {
 
-    public function index()
-    {
-        $array = array(
-            'error' => ''
-        );
+	public function index() {
+		$array = array(
+			'error' => ''
+		);
 
-        if (!empty($_SESSION['errorMsg'])) {
-            $array['error'] = $_SESSION['errorMsg'];
-            $_SESSION['errorMsg'] = '';
-        }
+		if(!empty($_SESSION['errorMsg'])) {
+			$array['error'] = $_SESSION['errorMsg'];
+			$_SESSION['errorMsg'] = '';
+		}
 
-        $this->loadView('login', $array);
-    }
+		$this->loadView('login', $array);
+	}
 
-    public function index_action()
-    {
-        if (!empty($_POST['email']) && !empty($_POST['password'])) {
-            $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-            $password = $_POST['password'];
+	public function index_action() {
+	
+		$email = (filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL) );
+		$password = (filter_input(INPUT_POST, 'password', FILTER_UNSAFE_RAW) );
+		$user = new Users();
 
-            $u = new Users();
-            if ($u->validateLogin($email, $password)) {
-                header("Location: " . BASE_URL);
-                exit;
-            } else {
-                $_SESSION['errorMsg'] = 'Usuário e/ou senha errados!';
-            }
-        } else {
-            $_SESSION['errorMsg'] = 'Preencha os campos abaixo.';
-        }
+		if ($email && $password && $user->validateLogin($email, $password)){
+			header('Location:' . BASE_URL);
+			exit;
+		}
+		$_SESSION['errorMsg'] = ($email && $password) ?
+								'Usuário e/ou senha incorretos' :
+								'Preencha os campos adequadamente';
+		header('Location:'. BASE_URL . 'Login');
+		exit;
+	}
+	
+	public function logout() {
 
-        header("Location: " . BASE_URL . "login");
-        exit;
-    }
+		unset($_SESSION['token']);
+		header("Location: ".BASE_URL);
+		exit;
 
-    public function logout()
-    {
+	}
 
-        unset($_SESSION['token']);
-        header("Location: " . BASE_URL);
-        exit;
-    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
